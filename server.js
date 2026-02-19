949832adc20f358383b7ff4255ffb1e1654d4a30
wasi226 <haiderwasi2263@gmail.com>
2026-02-19 23:05:33 +0530

    update hf router endpoint

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.post("/chat", async (req, res) => {
     }
 
     const response = await fetch(
-      "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2",
+      "https://router.huggingface.co/hf-inference/models/mistralai/Mistral-7B-Instruct-v0.2",
       {
         method: "POST",
         headers: {
